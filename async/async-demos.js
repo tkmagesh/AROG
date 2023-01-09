d84073f18ba5d8ca0d20fd63d06097b42db82862
Magesh Kuppan <tkmagesh77@gmail.com>
2023-01-09 19:53:11 +0530

    chaining promises

--- async/async-demos.js
+++ async/async-demos.js
@@ -35,11 +35,11 @@
     window['addAsyncClient'] = addAsyncClient
 
     function addAsyncPromise(x, y) {
-        console.log(`   [@service] processing ${x} and ${y}`)
+        console.log(`   [@service(add)] processing ${x} and ${y}`)
         var promise = new Promise(function(resolveFn, rejectFn){
             setTimeout(function () {
                 var result = x + y
-                console.log('   [@service] returning result')
+                console.log('   [@service(add)] returning result')
                 resolveFn(result)
             }, 4000)
         })
@@ -63,5 +63,26 @@
         console.log(`[@client] result = ${result}`)
     }
     window['addAsyncPromiseClient'] = addAsyncPromiseClient
+
+    function multiplyAsyncPromise(x, y) {
+        console.log(`   [@service(multiply)] processing ${x} and ${y}`)
+        var promise = new Promise(function (resolveFn, rejectFn) {
+            setTimeout(function () {
+                var result = x * y
+                console.log('   [@service(multiply)] returning result')
+                resolveFn(result)
+            }, 4000)
+        })
+        return promise;
+    }
+
+    async function multiPromiseClient() {
+        console.log(`[@client] triggering the add service`)
+        var addResult = await addAsyncPromise(100, 200)
+        console.log(`[@client] addResult = ${addResult}`)
+        var multiplyResult = await multiplyAsyncPromise(addResult, 2)
+        console.log(`[@client] multiplyResult = ${multiplyResult}`)
+    }
+    window['multiPromiseClient'] = multiPromiseClient
 
 })()
